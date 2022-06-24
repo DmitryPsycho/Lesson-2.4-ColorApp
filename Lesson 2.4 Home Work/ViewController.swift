6c4ae885a61229ad5c36b892fda16651a67e6ba1
DmitryPsycho <kovalenko2511@bk.ru>
2022-06-24 19:15:47 +0300

    Initial Commit

---
+++ Lesson 2.4 Home Work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.4 Home Work
+//
+//  Created by Psycho on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
