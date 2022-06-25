1bc47ad8895b57ea3cc67c0eb274645d5bc862cd
DmitryPsycho <kovalenko2511@bk.ru>
2022-06-25 15:29:37 +0300

    ColorApp creation.

--- Lesson 2.4 Home Work/ViewController.swift
+++ Lesson 2.4 Home Work/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupColorView()
     }
-
-
+    
+    @IBAction func colorChangeWithSliders() {
+        self.colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        )
+        
+        redValue.text = "\(round(redSlider.value * 100) / 100)"
+        greenValue.text = "\(round(greenSlider.value * 100) / 100)"
+        blueValue.text = "\(round(blueSlider.value * 100) / 100)"
+        
+    }
+    private func setupColorView() {
+        colorView.layer.cornerRadius = 30
+        self.colorView.backgroundColor = UIColor(
+            red: 0.5,
+            green: 0.5,
+            blue: 0.5,
+            alpha: 1.0
+        )
+        
+    }
 }
 
